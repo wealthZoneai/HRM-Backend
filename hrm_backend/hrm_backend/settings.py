1d74198d6b0ebba7bab46f0b42a324211417b0fb
Revanth Gurijala <revanth.wzg@gmail.com>
2025-11-11 10:57:58 +0530

    Removed old backend/frontend folders and added new hrm_backend project

--- hrm_backend/hrm_backend/settings.py
+++ hrm_backend/hrm_backend/settings.py
@@ -81,6 +81,12 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
