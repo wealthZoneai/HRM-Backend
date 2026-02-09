5e926a3f0893729bbc9579aa9293bc2c36dae528
Manogna <manogna.wzg@gmail.com>
2026-02-09 15:28:01 +0530

    Merged

---
+++ HRM/settings_dev.py
@@ -0,0 +1,34 @@
+import django
+from .settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = False
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'nagirikantiajay20@gmail.com'
+EMAIL_HOST_PASSWORD = 'khfj qsft sjeb tvax'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+#DATABASES = {
+ #  'default': {
+  ##      'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'api_testing',
+     #   'USER': 'postgres',
+      #  'PASSWORD': 'Dinesh@41216',
+       # 'HOST': '192.168.1.24',
+        #'PORT': '5432',
+    #}
+#}
