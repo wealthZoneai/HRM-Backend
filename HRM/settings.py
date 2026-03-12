1b38ea2cd1886895423804f67c3eaad1a20c7287
Ravi Teja <batturaviteja18@gmail.com>
2026-03-12 10:46:18 +0530

    faah

--- HRM/settings.py
+++ HRM/settings.py
@@ -162,13 +162,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_EXPOSE_HEADERS = ["Content-Type", "Content-Disposition"]
 
 CORS_ALLOWED_ORIGINS = []
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 CORS_ALLOW_HEADERS = [
     'accept',
--- manage.py
+++ manage.py
@@ -2,10 +2,9 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HRM.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HRM.settings_dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
